831b58d7e9bddc244fd3ce5c307e571bbfd788a8
crltsnch <crltsnch@myuax.com>
2021-12-12 17:52:26 +0100

    Crear funcion par acrear tableros y que los guarde en el fichero

---
+++ Ajedrez.py
@@ -0,0 +1,6 @@
+def partida_ajedrez(ajedrez):
+    tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
+    tablero =  []
+    for i in tablero_inicial.split('\n'):
+        tablero.append(i.split('\t'))
+    f = open(ajedrez, 'w')
