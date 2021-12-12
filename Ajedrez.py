99bffc5ab5dcb405842d13484fb886b06df2dfc2
crltsnch <crltsnch@myuax.com>
2021-12-12 18:14:32 +0100

    Recorrer el tablero

--- Ajedrez.py
+++ Ajedrez.py
@@ -1,6 +1,11 @@
 def partida_ajedrez(ajedrez):
     tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
-    tablero =  []
+    tablero=[]
     for i in tablero_inicial.split('\n'):
         tablero.append(i.split('\t'))
-    f = open(ajedrez, 'w')
+
+    f = open("ajedrez", 'w')
+    for i in partida_ajedrez:
+        f.write('\t'.join(i) + '\n')
+    f.close()
+    
