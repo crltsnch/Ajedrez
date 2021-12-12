6f7af95bf58b55c5ec4e39befbd3538a2dc19681
crltsnch <crltsnch@myuax.com>
2021-12-12 18:52:49 +0100

    Cerrar fichero y acabar el bucle

--- Ajedrez.py
+++ Ajedrez.py
@@ -1,10 +1,10 @@
-def partida_ajedrez(ajedrez):
+def partida_ajedrez(nombrefichero):
     tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
-    tablero =  []
+    tablero = []
     for i in tablero_inicial.split('\n'):
         tablero.append(i.split('\t'))
 
-    f = open("ajedrez", "w")
+    f = open(nombrefichero, "w")
     for i in partida_ajedrez:
         f.write('\t'.join(i) + '\n')
     f.close()
@@ -23,6 +23,10 @@ while True:
         tablero[filadestino-1][columnadestino-1] = tablero[filainicial-1][columnainicial-1]
         tablero[filainicial-1][columnainicial-1] = " "
         movimiento += 1
-        f = open("ajedrez", "a")
-        f.write('Movimiento' + str(movimiento) + '\n')
-        
+        f = open("nombrefichero", "a")
+        f.write("Movimiento" + str(movimiento) + '\n')
+        for i in tablero:
+            f.write('\t'.join(i) + '\n')
+        f.close()
+    return
+partida_ajedrez("partida1.txt")
