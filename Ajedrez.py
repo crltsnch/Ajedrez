ba2c76ba0bfd196b7cb21e7586f88a711266e7e2
crltsnch <crltsnch@myuax.com>
2021-12-12 18:41:53 +0100

    Inicio bucle para realizar movimientos

--- Ajedrez.py
+++ Ajedrez.py
@@ -8,4 +8,16 @@ def partida_ajedrez(ajedrez):
     for i in partida_ajedrez:
         f.write('\t'.join(i) + '\n')
     f.close()
-    
+
+movimiento = 0
+
+while True:
+    seguirjugando = input("Deseas hacer otro movimiento (si/no): ")
+    if seguirjugando == "no" :
+        break
+    else:
+        filainicial = int(input("Introduce la fila de la pieza a mover: "))
+        columnainicial = int(input("Introduce la columna de la pieza a mover: "))
+        filadestino = int(input("Introduce la fila de destino: "))
+        columnadestino = int(input("Introduce la columna de destino: "))
+        
